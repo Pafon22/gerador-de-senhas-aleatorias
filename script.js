c716692c16e0ee1a0d943d53e0b437d1bcb42dc2
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2023-08-24 10:03:49 -0300

    Testes de maiores e menores index

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const generatePassword = (size) => {
-const arrayNewPass = [];
-//caracteres específicos
-const possibleCharacter = 'abcdefghijklmnopqrstuvwxyz0123456789';
-// toString(36) -> /[0-9][a-z]/ (index: 0 _ 35)
+    // const arrayNewPass = [];
+    //caracteres específicos
+    // const possibleCharacter = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    // toString(36) -> /[0-9][a-z]/ (index: 0 _ 35)
 
 
-let maior = -Infinity;
-for(let index = 0; index < size; index +=1 ){
-    let newNumber = Math.floor(Math.random()*36);
-    if(maior < newNumber){
-        maior = newNumber
+    let maior = -Infinity;
+    let menor = Infinity;
+    for (let index = 0; index < size; index += 1) {
+        let newNumber = Math.floor(Math.random() * 36);
+        if (maior < newNumber) {
+            maior = newNumber
+        } else if (menor > newNumber) {
+            menor = newNumber;
+        }
     }
-}
 
 
 
-// newPass = newNumber.toString(36);
-// arrayNewPass.push(newNumber);
-return maior;
+    // newPass = newNumber.toString(36);
+    // arrayNewPass.push(newNumber);
+    return `Maior: ${maior} || Menor: ${menor}`;
 };
 
-console.log(generatePassword(35));
+for (let index = 0; index < 100; index += 1) {
+    console.log(generatePassword(35));
+}
