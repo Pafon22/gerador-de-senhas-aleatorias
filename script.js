0e66d18c91816673987729967adf0fb0d62a8461
Paulo Afonso Cardoso Franco - Turma 36 <paulo_afonso2006@hotmail.com>
2023-08-24 10:19:59 -0300

    Testes de UpperCase e LowerCase com push no array

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ const generatePassword = (size) => {
   const possibleCharacter = 'abcdefghijklmnopqrstuvwxyz0123456789';
   // toString(36) -> /[0-9][a-z]/ (index: 0 _ 35)
 
-  for (let index = 0; index < 100; index += 1) {
-    let newNumber = Math.floor(Math.random() * 36);
+  for (let index = 0; index < size; index += 1) {
+    let newNumber = Math.floor(Math.random() * 36).toString(36);
     let randomCaps = Math.floor(Math.random() * 2);
-    arrayNewPass.push(newNumber.toString(36))
-    console.log('index: ' + index + ' || randomCaps: '+ randomCaps + ' || ' + Boolean(randomCaps));
+    if(randomCaps){
+      newNumber = newNumber.toUpperCase();
+    };
+    console.log('i: ' + index + ' || rC: ' + randomCaps + ' || nN: ' + newNumber);
+    arrayNewPass.push(newNumber);
   }
-
+console.log(arrayNewPass);
 
 
   // newPass = newNumber.toString(36);
